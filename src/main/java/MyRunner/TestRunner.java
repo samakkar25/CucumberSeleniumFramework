8ce1418ef05b62097645a504af143e20166d4de2
siteshag <31569420+siteshag@users.noreply.github.com>
2019-09-22 22:56:51 -0400

    initial draft

--- src/main/java/MyRunner/TestRunner.java
+++ src/main/java/MyRunner/TestRunner.java
@@ -1,15 +1,18 @@
 package MyRunner;
 
 import org.junit.runner.RunWith;
+
 import cucumber.api.CucumberOptions;
 import cucumber.api.junit.Cucumber;
 
 
+
 	@RunWith(Cucumber.class)
 	@CucumberOptions(
-			features = "/Users/NaveenKhunteta/Documents/workspace/FreeCrmBDDFramework/src/main/java/Features/taggedhooks.feature", //the path of the feature files
+			//features = "/Users/NaveenKhunteta/Documents/workspace/FreeCrmBDDFramework/src/main/java/Features/taggedhooks.feature", //the path of the feature files
+			features = "D:/Selenium_Data2/CucumberSeleniumFramework/src/main/java/Features/login.feature",
 			glue={"stepDefinitions"}, //the path of the step definition files
-			format= {"pretty","html:test-outout", "json:json_output/cucumber.json", "junit:junit_xml/cucumber.xml"}, //to generate different types of reporting
+			//format= {"pretty","html:test-outout", "json:json_output/cucumber.json", "junit:junit_xml/cucumber.xml"}, //to generate different types of reporting
 			monochrome = true, //display the console output in a proper readable format
 			strict = true, //it will check if any step is not defined in step definition file
 			dryRun = false //to check the mapping is proper between feature file and step def file
